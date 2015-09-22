a15a259f155bbadfc42a19bdc9172351ada2e9ad
aaronjudd <aaron@ongoworks.com>
2015-09-21 22:13:01 -0700

    updated package.js

--- package.js
+++ package.js
@@ -1,18 +1,37 @@
 Package.describe({
   summary: "Reaction Braintree - Braintree payments for Reaction Commerce",
   name: "reactioncommerce:reaction-braintree",
-  version: "1.3.0",
+  version: "1.5.0",
   git: "https://github.com/reactioncommerce/reaction-braintree.git"
 });
 
-Npm.depends({'braintree': '1.27.0'});
+Npm.depends({'braintree': '1.29.0'});
 
 Package.onUse(function (api, where) {
-  api.versionsFrom('METEOR@1.0');
-  api.use("meteor-platform@1.2.2");
-  api.use("coffeescript");
+  api.versionsFrom('METEOR@1.2');
+
+  // meteor base packages
+  api.use("standard-minifiers");
+  api.use("mobile-experience");
+  api.use("meteor-base");
+  api.use("mongo");
+  api.use("blaze-html-templates");
+  api.use("session");
+  api.use("jquery");
+  api.use("tracker");
+  api.use("logging");
+  api.use("reload");
+  api.use("random");
+  api.use("ejson");
+  api.use("spacebars");
+  api.use("check");
+
+  // meteor add-on packages
   api.use("less");
-  api.use("reactioncommerce:core@0.6.1");
+  api.use("coffeescript");
+
+  // use reaction commerce
+  api.use("reactioncommerce:core@0.8.0");
 
   api.addFiles("server/register.coffee",["server"]); // register as a reaction package
   api.addFiles("server/braintree.coffee",["server"]);
